404e005752ce1a03765ba883ef4c440224f58b12
ProjetoBoxe <163341157+ProjetoBoxe@users.noreply.github.com>
2024-06-21 23:55:13 -0300

    strings

--- anotações.py
+++ anotações.py
@@ -53,3 +53,72 @@ print(float('1') + 1)
 print(str(1) + 'e')
 
 
+#Variaveis
+
+nome = 'lucas'
+idade = 20
+maior_de_idade = idade >= 18
+print('nome:', nome ,'idade:', idade)
+print(maior_de_idade)
+
+
+#Peculiaridades matematica
+
+
+divisao = 10 / 3 #float
+print(divisao)
+divisao_inteira = 10 // 3
+print(divisao_inteira)
+exponenciacao = 2 ** 10
+print(exponenciacao)
+
+
+#concatenacao e repeticao
+
+
+concatenacao = 'Lucas ' + 'Ferreira ' + 'de ' + 'Almeida'
+print(concatenacao)
+a_dez_vezes = 'a' * 10
+print(a_dez_vezes)
+
+
+#Formatacao de strings
+
+
+nome = 'lucas'
+altura = 1.71
+dinheiro = 10000000.5
+peso = 69
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.1f} de altura' # :.(numero)f   serve para voce decidir quantos numeros apos a ponto voce quer mostrar
+print(linha_1)
+linha_2 = f'{nome} tem {altura:.3f} de altura'
+print(linha_2)
+linha_3 = f'{nome} tem {dinheiro:,.3f} de altura' # :,.(numero)f   serve para voce seperar as casas
+print(linha_3)
+
+
+#formatação de strings
+
+a = 'AAAAA'
+b = 'BBBBB'
+c = 1.5
+string = 'b={1} a={0} b={1} c={2:.2f} c={2:.5f}'
+formato = string.format(a,b,c) # a = INDICE 0 b = INDICE 1 c = INDICE 2(que nem vetor)
+print(formato)
+
+
+#input
+
+
+nome = input('qual é o seu nome?')
+print(f'o seu nome é{nome=}')
+
+numero_1=input('me fale o primeiro numero')
+numero_2=input('me fale o segundo numero')
+
+int_numero_1=int(numero_1)
+int_numero_2=int(numero_2)
+
+print(f'a soma dos numeros é:{int_numero_1 + int_numero_2}')
