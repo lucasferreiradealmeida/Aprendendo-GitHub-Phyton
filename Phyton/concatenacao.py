fdd6e34110f8713233a1890aa0f0afaffb52c759
lucasferreiradealmeida <lucasfalmeida07@gmail.com>
2024-06-24 17:47:20 -0300

    mudanças nas anotações

---
+++ Phyton/concatenacao.py
@@ -0,0 +1,4 @@
+concatenacao = 'Lucas ' + 'Ferreira ' + 'de ' + 'Almeida'
+print(concatenacao)
+a_dez_vezes = 'a' * 10
+print(a_dez_vezes)
---
+++ Phyton/print.py
@@ -0,0 +1,4 @@
+print(7,10,2003, sep="-", end="\n")
+print("Licas")
+print(10,2003, sep="-", end="##")
+print("Lucas")
