c9acae22451235610bcccbf09ab20a28267df0be
lucasferreiradealmeida <lucasfalmeida07@gmail.com>
2024-07-10 18:10:40 -0300

    exercicio7

--- Exercicios/exercicio7.py
+++ Exercicios/exercicio7.py
@@ -15,3 +15,15 @@ Faça a contagem de tentativas do seu
 usuário.
 """
 
+palavra_secreta = 'Lucas'
+
+while True:
+    letra_digitada = input("Digite uma letra:")
+    if len(letra_digitada) != 1 and letra_digitada != ' ':
+        print('Digite apenas uma letra')
+    else:
+        if letra_digitada in palavra_secreta:
+            letra_acertadas += letra_digitada
+        for letra_secreta in palavra_secreta:
+            print(letra_secreta)
+        
